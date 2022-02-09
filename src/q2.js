6ce199c1829e98398d25ea8382846d37492ab68f
BrandonV21 <brandon00095@gmail.com>
2022-02-09 14:12:09 +0000

    q2

---
+++ src/q2.js
@@ -0,0 +1,12 @@
+function myFunction(num1, num2) {
+    num1+num2
+  }
+  
+  const result = myFunction(5,5)
+
+  /*
+  What is the value of result after calling this function? Why?
+  Similar to q1, but instead, it's not returning anything. Therefore the result variable will be undefined. 
+
+  As expected, when I typed the console log command, it returned undefined. 
+  */
